f4444ae1e821772a7aa423c4db9c608063cc3322
Giovanni Bryden <giovanni.bryden@moovweb.com>
2014-04-10 17:02:19 -0700

    Got users to work finally. Passwords and whatnot

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SousMates::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   resources :posts
 
   root 'static#index'
   get '/about' => 'static#about'
   get '/contact' => 'static#contact'
+  get '/login' => 'sessions#new'
 
 end
