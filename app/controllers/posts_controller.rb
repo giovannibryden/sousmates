aa92c26cfc3d1670f1116f8cfae83af4ed3527db
Giovanni Bryden <giovanni.bryden@moovweb.com>
2014-04-18 11:58:08 -0700

    Minor changes to links in header

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :confirm_logged_in, :except => [:show, :index]
 
